e981db4cdbbd43e1f5a8ac6790c83834e5499712
TEKBAS Kenan <tekbask8@gmail.com>
2026-04-22 23:28:56 +0200

    feat: compose redesigned portfolio page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Links from '@/components/Links'
+import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 import Footer from '@/components/Footer'
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <About />
       <Links />
+      <Projects />
       <Contact />
       <Footer />
     </main>
